2d4dd11fc3bf343f46b19a0bd5741aad4ab10766
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-12-02 01:45:34 +0200

    all tasks done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,71 @@
-// const key = new Key();
+class Key {
+  private signature: number;
 
-// const house = new MyHouse(key);
-// const person = new Person(key);
+  constructor() {
+    this.signature = Math.random();
+  }
 
-// house.openDoor(person.getKey());
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-// house.comeIn(person);
+class Person {
+  private key: Key;
+  
+constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  key: Key;
+  door: boolean;
+  tenants: Person[] = [];
+  abstract openDoor(key: Key): void;
+
+ constructor(key: Key) {
+   this.key = key;
+   this.door = false;
+  }
+
+  comeIn(person: Person): void {
+   if (this.door) {
+     this.tenants.push(person);
+      console.log("Wellcome Home")
+   } else {
+     console.log("Wrong key, you can't come in");
+     }
+  }
+}
 
 
-// export {};
+class MyHouse extends House {
+ 
+  openDoor(key:Key):void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is opened");
+    } else {
+      this.door = false;
+      console.log("The door is closed");
+      
+    }
+  }
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+
+export {};
