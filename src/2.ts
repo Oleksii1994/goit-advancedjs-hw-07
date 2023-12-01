12618288601f24a0087f46170064018af116e8c2
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-12-02 00:41:31 +0200

    first two tasks done

--- src/2.ts
+++ src/2.ts
@@ -16,11 +16,11 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+ public name: string;
+ private department: string;
+ protected salary: number;
 
-  constructor(name: string, department: string, salary: number) {
+  constructor( name: string, department: string, salary: number) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -33,7 +33,14 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+    this.salary += 10000;
+  }
 }
+
+const manager = new Manager("Alex", "Marketing", 20000);
+console.log(manager.getEmployeeDetails());
 
 
 export {};
